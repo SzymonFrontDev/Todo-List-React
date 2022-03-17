ba773a6fdb318555d168042ff790eb13fe3deab6
Szymon Lesko <leskoszymon30@gamil.com>
2022-03-17 17:28:08 +0100

    removed unnecessary things

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,11 +2,10 @@ import "./style.css";
 
 
 const Form = () => (
-    <form className="form">
-          <input className="form__newTask" placeholder="Co jest do zrobienia?" />
-          <button className="form__button">Dodaj zadanie</button>
-        </form>
+  <form className="form">
+    <input className="form__newTask" placeholder="Co jest do zrobienia?" />
+    <button className="form__button">Dodaj zadanie</button>
+  </form>
 );
-
 
 export default Form;

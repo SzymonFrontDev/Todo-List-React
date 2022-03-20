587431b479b4c834e1004aeca9f46243b5d72a3e
Szymon Lesko <leskoszymon30@gamil.com>
2022-03-20 21:24:08 +0100

    changed title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Form from "./Form";
 import Tasks from "./Tasks";
-import Buttons from"./Buttons";
+import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
 
-const tasks =[
+const tasks = [
   { id: 1, content: "przejść na Reacta", done: false },
   { id: 2, content: "zjeść obiad", done: true },
 ];
@@ -17,22 +17,23 @@ function App() {
     <Container>
       <Header title="Lista zadań" />
 
-      <Section 
-      title="Dondaj nowe zadanie" 
-      body={<Form />} 
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
       />
-      <Section 
-      title="Lista zadań"
-      body={
-      <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />
-    }
-      extraHeaderContent={
-      <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks}/>
-    } 
+      <Section
+        title="Lista zadań"
+        body={
+          <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />
+        }
+        extraHeaderContent={
+          <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
+        }
       />
 
-    
+
     </Container>
-  )}
+  )
+}
 
 export default App;

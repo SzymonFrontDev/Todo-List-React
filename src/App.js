f67ccccd074b355e2bf1f2241cd27f76aa8b73cf
Szymon Lesko <leskoszymon30@gamil.com>
2022-07-17 09:38:48 +0200

    added css to NavLinkList

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import { HashRouter, Switch, Route, Redirect,} from "react-router-dom";
 import TasksPage from "./features/tasks/TasksPage"
 import TaskPage from "./features/tasks/TaskPage"
 import AuthorPage from "./features/tasks/author/AuthorPage";
-import { StyledNavLink } from "./styled.js";
+import { StyledNavLink, NavLinkList } from "./styled.js";
 
 
 export default () => (
   <HashRouter>
     <nav>
-      <ul>
+      <NavLinkList>
         <li>
           <StyledNavLink to="/zadania">Zadania</StyledNavLink>
         </li>
         <li>
           <StyledNavLink to="/autor">O autorze</StyledNavLink>
         </li>
-      </ul>
+      </NavLinkList>
       <Switch>
       <Route path="/zadania/:id">
           <TaskPage />
--- src/styled.js
+++ src/styled.js
@@ -1,12 +1,27 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-const activeClassName = "active";
-
-export const StyledNavLink = styled(NavLink).attrs(() => ({
-    activeClassName,
-}))`
-&.${activeClassName} {
-    color: red;
+export const StyledNavLink = styled(NavLink)`
+text-decoration: none;
+color: #fff;
+&.active {
+    color:#fff;
+    font-weight: bolder;
+    text-decoration: none;
 }
+`
+
+export const NavLinkList = styled.ul`
+  font-size: 20px;
+  text-decoration: none;
+  list-style-type: none;
+  background-color:teal;
+  margin-top: 0;
+  padding: 18px;
+  color: #fff;
+  text-align: center;
+  display:flex;
+  flex-direction: row;
+  justify-content: center;
+  gap :40px
 `
